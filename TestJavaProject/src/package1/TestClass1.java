905c77a2fcb77faafabf71a27ee4f66564e3bb32
Swaroop Nadella <swaroopnadella@hotmail.com>
2024-08-04 17:37:33 +0530

    second commit

--- TestJavaProject/src/package1/TestClass1.java
+++ TestJavaProject/src/package1/TestClass1.java
@@ -5,6 +5,7 @@ public class TestClass1 {
 	public static void main(String[] args) {
 		
 		System.out.println("Swaroop Nadella");
+		System.out.println("Test Message");
 	}
 
 }
--- TestJavaProject/src/package1/TestClass2.java
+++ TestJavaProject/src/package1/TestClass2.java
@@ -4,6 +4,7 @@ public class TestClass2 {
 
 	public static void main(String[] args) {
 		System.out.println("Second Class File");
+		System.out.println("Test");
 	}
 
 }
