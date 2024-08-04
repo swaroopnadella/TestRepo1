15af97719cccd508a8dd5e894beb180744cfd461
Swaroop Nadella <swaroopnadella@hotmail.com>
2024-08-04 17:32:59 +0530

    First commit

---
+++ TestJavaProject/src/package1/TestClass1.java
@@ -0,0 +1,10 @@
+package package1;
+
+public class TestClass1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Swaroop Nadella");
+	}
+
+}
---
+++ TestJavaProject/src/package1/TestClass2.java
@@ -0,0 +1,9 @@
+package package1;
+
+public class TestClass2 {
+
+	public static void main(String[] args) {
+		System.out.println("Second Class File");
+	}
+
+}
